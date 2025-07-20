d4f1d69a727442a68bae065d5967056868319093
Tofikbe <karategamingyt@gmail.com>
2025-07-20 17:07:12 +0530

    Update EcosystemProjects.jsx

--- src/components/EcosystemProjects.jsx
+++ src/components/EcosystemProjects.jsx
@@ -1,1 +1,14 @@
+import React from "react";
 
+export default function EcosystemProjects() {
+  return (
+    <div>
+      <h2>Aptos Ecosystem Projects</h2>
+      <ul>
+        <li>Project 1</li>
+        <li>Project 2</li>
+        <li>Project 3</li>
+      </ul>
+    </div>
+  );
+}
