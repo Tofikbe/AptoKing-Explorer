88c16c842766d4aec1271286add90a49bdab51ad
Tofikbe <karategamingyt@gmail.com>
2025-07-20 17:07:32 +0530

    Update TVL.jsx

--- src/components/TVL.jsx
+++ src/components/TVL.jsx
@@ -1,1 +1,10 @@
+import React from "react";
 
+export default function TVL() {
+  return (
+    <div>
+      <h2>Total Value Locked</h2>
+      <p>$123,456,789 (Live API Add Here)</p>
+    </div>
+  );
+}
