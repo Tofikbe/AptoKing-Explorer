0ec4629ed49ccfcc17749db7a8db98186ee95ba6
Tofikbe <karategamingyt@gmail.com>
2025-07-20 17:07:54 +0530

    Update AptosPrice.jsx

--- src/components/AptosPrice.jsx
+++ src/components/AptosPrice.jsx
@@ -1,1 +1,10 @@
+import React from "react";
 
+export default function AptosPrice() {
+  return (
+    <div>
+      <h2>APT Token Price</h2>
+      <p>Price: $5.42 (Live API Add Here)</p>
+    </div>
+  );
+}
