161c1e064d1627448f62407c46f4b596af31e9d2
Tofikbe <karategamingyt@gmail.com>
2025-07-20 17:06:51 +0530

    Update TwitterFeed.jsx

--- src/components/TwitterFeed.jsx
+++ src/components/TwitterFeed.jsx
@@ -1,1 +1,12 @@
+import React from "react";
 
+export default function TwitterFeed() {
+  return (
+    <div>
+      <h2>My Twitter Feed</h2>
+      <a href="https://twitter.com/YourTwitterHandle" target="_blank" rel="noreferrer">
+        Go to my Twitter
+      </a>
+    </div>
+  );
+}
